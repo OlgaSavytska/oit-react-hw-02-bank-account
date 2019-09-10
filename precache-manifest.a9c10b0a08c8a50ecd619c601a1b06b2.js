a5bd049f8b1be05f5597956965cb8335fc20cccc
OlgaSavytska <o.savytska77@gmail.com>
2019-09-10 15:14:43 +0300

    Updates

---
+++ precache-manifest.a9c10b0a08c8a50ecd619c601a1b06b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7f3c2350471e471b3296d6f45d7c79",
+    "url": "/oit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "52d65a0ff42bf3aa1fa7",
+    "url": "/oit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "a35fa284ab7cbe5aeb8b",
+    "url": "/oit-react-hw-02-bank-account/static/css/main.3148d4ca.chunk.css"
+  },
+  {
+    "revision": "52d65a0ff42bf3aa1fa7",
+    "url": "/oit-react-hw-02-bank-account/static/js/2.ff3578ed.chunk.js"
+  },
+  {
+    "revision": "a35fa284ab7cbe5aeb8b",
+    "url": "/oit-react-hw-02-bank-account/static/js/main.4b94f849.chunk.js"
+  },
+  {
+    "revision": "af60a58f00c6208a607a",
+    "url": "/oit-react-hw-02-bank-account/static/js/runtime~main.820a4f18.js"
+  }
+]);
